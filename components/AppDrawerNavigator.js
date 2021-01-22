92780c15ea88df530e33b80af1241253d2f0b7e5
vasudha921 <61460922+vasudha921@users.noreply.github.com>
2021-01-22 16:03:09 +0530

    Add files via upload

--- components/AppDrawerNavigator.js
+++ components/AppDrawerNavigator.js
@@ -3,11 +3,14 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import App from '../App';
 import { AppTabNavigator } from './AppTabNavigator'
 import CustomSideBarMenu  from './CustomSideBarMenu';
-
+import SettingScreen from '../screens/SettingScreen';
 export const AppDrawerNavigator = createDrawerNavigator({
     Home:{
         screen:AppTabNavigator
     },
+    Setting :{
+        screen:SettingScreen
+    }
  },
     {
          contentComponent: CustomSideBarMenu
